101348676863dae86d8a6f7dd42022c4af674e49
Bruno Eris <brunoeriss@gmail.com>
2015-10-05 19:48:58 -0300

    bootstrap and jquery

--- Hercilio-BrunoEris/app/user_login.php
+++ Hercilio-BrunoEris/app/user_login.php
@@ -4,17 +4,26 @@ if($_SERVER['REQUEST_METHOD'] == 'POST'){
     UserLogin($_POST['email'], $_POST['password']);
 }
 ?>
+
 <!DOCTYPE html>
-<html lang="pt-br">
-<head>
+<html>
+  <head>
     <meta charset="utf-8">
     <title>Pão na Mão</title>
-<body>
-<form class="form-signin" method="post" action="">
-    <input type="email" name="email" placeholder="Usuário" required autofocus>
-    <input type="password" name="password" placeholder="Senha" required>
-    <button type="submit">Entrar</button>
-    <a href="recover.php" title="Esqueci a senha">Esqueci a senha</a>
-</form>
-</body>
+    <link rel="stylesheet" href="assets/css/bootstrap.css" media="screen" charset="utf-8">
+  </head>
+  <body>
+    <div class="container">
+      <h1>Olá!</h1>
+      <form class="form-signin" method="post" action="">
+          <input type="email" name="email" placeholder="Usuário" required autofocus>
+          <input type="password" name="password" placeholder="Senha" required>
+          <button type="submit">Entrar</button>
+          <a href="recover.php" title="Esqueci a senha">Esqueci a senha</a>
+      </form>
+    </div>
+    
+    <script src="assets/js/jquery-1.11.3.min.js"></script>
+    <script src="assets/js/bootstrap.min.js"></script>
+  </body>
 </html>
--- Hercilio-BrunoEris/app/user_register.php
+++ Hercilio-BrunoEris/app/user_register.php
@@ -4,19 +4,28 @@ if($_SERVER['REQUEST_METHOD'] == 'POST'){
     UserLogin($_POST['name'], $_POST['email'], $_POST['telephone'], $_POST['password'], $_POST['password_confirm']);
 }
 ?>
+
 <!DOCTYPE html>
-<html lang="pt-br">
-<head>
+<html>
+  <head>
     <meta charset="utf-8">
     <title>Pão na Mão</title>
-<body>
-<form class="form-signin" method="post" action="">
-    <input type="text" maxlength="255" name="name" placeholder="Nome" required autofocus>
-    <input type="email" maxlength="255" name="email" placeholder="E-mail" required autofocus>
-    <input type="text" maxlength="11" name="telephone" placeholder="Telefone" required autofocus>
-    <input type="password" name="password" placeholder="Senha" required>
-    <input type="password" name="password_confirm" placeholder="Confirme a Senha" required>
-    <button type="submit">Cadastrar</button>
-</form>
-</body>
+    <link rel="stylesheet" href="assets/css/bootstrap.css" media="screen" charset="utf-8">
+  </head>
+  <body>
+    <div class="container">
+      <h1>Registre-se</h1>
+      <form class="form-signin" method="post" action="">
+          <input type="text" maxlength="255" name="name" placeholder="Nome" required autofocus>
+          <input type="email" maxlength="255" name="email" placeholder="E-mail" required autofocus>
+          <input type="text" maxlength="11" name="telephone" placeholder="Telefone" required autofocus>
+          <input type="password" name="password" placeholder="Senha" required>
+          <input type="password" name="password_confirm" placeholder="Confirme a Senha" required>
+          <button type="submit">Cadastrar</button>
+      </form>
+    </div>
+
+    <script src="assets/js/jquery-1.11.3.min.js"></script>
+    <script src="assets/js/bootstrap.min.js"></script>
+  </body>
 </html>
