245e8093dc66301fcecc9043e8e6c1571cbeb948
Bruno Eris <brunoeriss@gmail.com>
2015-10-10 23:45:21 -0300

    basic screen comp_register, user_login, user_register

--- Hercilio-BrunoEris/app/user_login.php
+++ Hercilio-BrunoEris/app/user_login.php
@@ -25,8 +25,8 @@
                   <input type="password" name="password" class="form-control" placeholder="Senha" required>
                   <button class="btn btn-lg btn-primary btn-block" type="submit">Entrar</button>
                   <a class="btn btn-sm btn-link" href="user_recover.php" title="Esqueci a senha">Esqueci a senha</a><br><br>
-                  <a class="btn btn-sm btn-default" href="#">Cadastre-se</a><br>
-                  <small><a class="btn btn-sm btn-link btn-success" href="#">Empresa? Cadastre-se aqui!</a></small>
+                  <a class="btn btn-sm btn-default" href="user_register.php">Cadastre-se</a><br>
+                  <small><a class="btn btn-sm btn-link btn-success" href="comp_register.php">Empresa? Cadastre-se aqui!</a></small>
                 </form>
             </div>
         </div>
