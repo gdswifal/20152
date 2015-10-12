0ea9ccf7bd8de2fd5cd64530fcaffed5e91b839e
Hernior <hercilio.master@gmail.com>
2015-10-12 19:57:12 -0300

    aesthetic correction

--- Hercilio-BrunoEris/app/views/feedback.php
+++ Hercilio-BrunoEris/app/views/feedback.php
@@ -1,20 +1,20 @@
 <?php
 switch (@$_GET["status"]){
-    case '6c6f676f7574':
+    case '6c6f676f7574': //logout
         echo '
         <div class="alert alert-success alert-dismissible" role="alert">
             <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
             Sessão encerrada com sucesso.
         </div>';
         break;
-    case '77726f6e6720757365726e616d65206f722070617373776f7264':
+    case '77726f6e6720757365726e616d65206f722070617373776f7264': //wrong username or password
         echo '
         <div class="alert alert-danger alert-dismissible" role="alert">
             <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
             <strong>Atenção!</strong> Dados de acesso incorretos.
         </div>';
         break;
-    case '6e6f7420666f756e64':
+    case '6e6f7420666f756e64': //not found
         echo '
         <div class="alert alert-warning alert-dismissible" role="alert">
             <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
