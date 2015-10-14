31f6ace5be5fd625269c2c4b4c15b6a43e05740c
Bruno Eris <brunoeriss@gmail.com>
2015-10-14 19:51:18 -0300

    new path ico

--- Hercilio-BrunoEris/app/assets/js/map_user_main.js
+++ Hercilio-BrunoEris/app/assets/js/map_user_main.js
@@ -34,7 +34,7 @@ function initialize() {
             var marker = new google.maps.Marker({
                 position: myLatlng,
                 map: map,
-                icon: '../../assets/img/coffee-shop2.png'
+                icon: '../../assets/img/coffee-shop.png'
             });
 
             var conteudo = '<div><img src="../../images/companyLogos/'+val.logo+'" alt="SEM FOTO" class="logoCompany">'+
