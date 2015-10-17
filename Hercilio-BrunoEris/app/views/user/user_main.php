42c7b081e711e1de971c0c94492927c66ba7bac9
Bruno Eris <brunoeriss@gmail.com>
2015-10-17 18:19:26 -0300

    reposition nav elements

--- Hercilio-BrunoEris/app/views/user/user_main.php
+++ Hercilio-BrunoEris/app/views/user/user_main.php
@@ -13,7 +13,6 @@
     <header>
       <nav class="navbar navbar-inverse">
         <div class="container-fluid">
-          <!-- Brand and toggle get grouped for better mobile display -->
           <div class="navbar-header">
             <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
               <span class="sr-only">Toggle navigation</span>
@@ -21,14 +20,18 @@
               <span class="icon-bar"></span>
               <span class="icon-bar"></span>
             </button>
-            <a class="navbar-brand" href="#">Pão na Mão</a>
+            <a class="navbar-brand brand-main" href="#">
+              <span>Pão na Mão <img class="brand-pao" src="../../assets/img/bread-ico.png" alt="" /></span>
+            </a>
           </div>
 
-          <!-- Collect the nav links, forms, and other content for toggling -->
           <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
             <ul class="nav navbar-nav navbar-right">
               <li class="dropdown">
-                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Olá, <?php echo $_SESSION['name'];?>! <span class="caret"></span></a>
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
+                  <img class="user-avatar" src="../../assets/img/avatar_default.png" alt="avatar" />
+                    &nbsp Olá, <?php echo $_SESSION['name'];?>! <span class="caret"></span>
+                </a>
                 <ul class="dropdown-menu">
                   <li><a href="#">Meus Pedidos</a></li>
                   <li><a href="#">Atualizar Dados</a></li>
@@ -37,16 +40,17 @@
                 </ul>
               </li>
             </ul>
-            <form class="navbar-form navbar-right" role="search">
+            <form class="navbar-form user-search navbar-right" role="search">
               <div class="form-group">
-                <input type="text" class="form-control" placeholder="Padaria..">
+                <input type="text" class="form-control input-sm" placeholder="Padaria..">
               </div>
-              <button type="submit" class="btn btn-default">Pesquisar</button>
+              <button type="submit" class="btn btn-sm btn-default">Pesquisar</button>
             </form>
           </div>
         </div>
       </nav>
     </header>
+
     <div class="container-fluid">
       <div id="map" style="height:600px;margin:auto;"></div>
     </div>
