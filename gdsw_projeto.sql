1a641d2823e9a23154573a0b2e4e73148ddfc70a
Bruno Antonelly <antonnelly@gmail.com>
2015-10-30 00:14:08 -0300

    Correção do banco
    Correções no SQL.

--- gdsw_projeto.sql
+++ gdsw_projeto.sql
@@ -1,65 +1,64 @@
--- phpMyAdmin SQL Dump
--- version 4.2.11
--- http://www.phpmyadmin.net
 --
--- Host: 127.0.0.1
--- Generation Time: 22-Out-2015 às 19:33
--- Versão do servidor: 5.6.21
--- PHP Version: 5.6.3
+-- Cria o banco de dados
+--
+CREATE DATABASE GDSW_PROJETO DEFAULT CHARSET=latin1;
+--
+-- Cria a tabela TIPO_USUARIO
+--
+CREATE TABLE IF NOT EXISTS TIPO_USUARIO (
+	ID INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,	-- Código INTEGERerno (PK)
+	TIPO VARCHAR(32) NOT NULL,			-- Descrição
+	PRIMARY KEY(ID)					-- Define o campo ID como PK (Primary Key)
+);
+--
+-- Cria a tabela USUARIOS
+--
+CREATE TABLE IF NOT EXISTS USUARIOS (
+	ID INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,		-- ID usuário (PK)
+	LOGIN VARCHAR(64) NOT NULL UNIQUE,			-- Nome com até 64 caracteres
+	SENHA VARCHAR(9) NOT NULL,				-- Telefone, podendo ser nulo caso não tenha
+	TIPO INTEGER UNSIGNED NOT NULL,					-- Tipo de usuário
+	PRIMARY KEY(ID),						-- Define o campo ID como PK (Primary Key)
+	FOREIGN KEY(TIPO) REFERENCES TIPO_USUARIO(ID)		-- Referencia a tabela TIPO_USUARIOS
+);
+--
+-- Cria a tabela EXT_VOCAL
+--
+CREATE TABLE IF NOT EXISTS EXT_VOCAL (
+	ID INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,		-- Código INTEGERerno (PK)
+	TIPO VARCHAR(64) NOT NULL,				-- Nome com até 64 caracteres
+	PRIMARY KEY(ID)						-- Define o campo ID como PK (Primary Key)
+);
+--
+-- Cria a tabela MEMBROS
+--
+CREATE TABLE IF NOT EXISTS MEMBROS (
+	ID INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,		-- ID dos membros (PK)
+	NOME VARCHAR(64) NOT NULL,				-- Nome com até 64 caracteres
+	FONE INTEGER(9) UNSIGNED NOT NULL,			-- Telefone de contato
+	EMAIL VARCHAR(64) NULL,					-- E-mail dos membros
+	EX_VOCAL INTEGER UNSIGNED NOT NULL,					-- Tipo da extensão vocal
+	PRIMARY KEY(ID),					-- Define o campo ID como PK (Primary Key)
+	FOREIGN KEY(EX_VOCAL) REFERENCES EXT_VOCAL(ID)		-- Cria o relacionamento (FK) com a tabela TIPO
+);								
+--
+-- Cria a tabela TIPO_EVENTO
+--
+CREATE TABLE IF NOT EXISTS TIPO_EVENTO (
+	ID INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,		-- Código INTEGERerno (PK)
+	TIPO VARCHAR(64) NOT NULL,				-- Código do tipo
+	PRIMARY KEY(ID)					-- Define o campo CODIGO como PK (Primary Key)
+);
+--
+-- Cria a tabela EVENTOS
+--
+CREATE TABLE IF NOT EXISTS EVENTOS (
+	ID INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,		-- ID do evento
+	LOCAL VARCHAR(64) NOT NULL,				-- Local do evento
+	HORA_DATA DATETIME,					-- Data e hora do evento
+	PARTICIPANTES INTEGER UNSIGNED NOT NULL,			-- Participantes do evento
+	TIPO INTEGER UNSIGNED NOT NULL,				-- Tipo do evento
+	PRIMARY KEY(ID),					-- Define ID como chave primária
+	FOREIGN KEY(TIPO) REFERENCES TIPO_EVENTO(ID)		-- Cria o relacionamento (FK) com a tabela TIPO_EVENTO
+);
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-
---
--- Database: `gdsw_projeto`
---
-
--- --------------------------------------------------------
-
---
--- Estrutura da tabela `usuarios`
---
-
-CREATE TABLE IF NOT EXISTS `usuarios` (
-`id` int(10) unsigned NOT NULL,
-  `nome` varchar(100) NOT NULL,
-  `usuario` varchar(50) NOT NULL,
-  `senha` varchar(50) NOT NULL
-) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
-
---
--- Extraindo dados da tabela `usuarios`
---
-
-INSERT INTO `usuarios` (`id`, `nome`, `usuario`, `senha`) VALUES
-(1, '', 'lucas', 'e10adc3949ba59abbe56e057f20f883e'),
-(2, '', 'usuario', '123456');
-
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `usuarios`
---
-ALTER TABLE `usuarios`
- ADD PRIMARY KEY (`id`), ADD UNIQUE KEY `usuario` (`usuario`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `usuarios`
---
-ALTER TABLE `usuarios`
-MODIFY `id` int(10) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
