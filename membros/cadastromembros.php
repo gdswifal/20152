e9ff0567bf7c0a6fb5191fb18ca7dd571ce08d87
unknown <Lucas.gabriel@l1m08.informatica.ifal.edu.br>
2015-11-04 17:17:19 -0300

    Começo do formulário de cadastro

---
+++ membros/cadastromembros.php
@@ -0,0 +1,47 @@
+<?php
+ 
+$nome = $_POST['nome'];
+$idade = ($_POST['idade']);
+$telefone = $_POST['telefone'];
+$idade = ($_POST['idade']);
+$endereco = $_POST['endereco'];
+$extvocal = $_POST['extvocal'];
+$connect = mysql_connect('localhost','root','');
+$db = mysql_select_db('gdsw_projeto');
+$query_select = "SELECT nome FROM membros WHERE nome = '".$nome."'";
+$select = mysql_query($query_select,$connect);
+$array = mysql_fetch_array($select);
+$logarray = $array['nome'];
+
+$sql = mysql_query("INSERT INTO membros (nome,telefone,idade,ex_vocal,endereco) VALUES ('$nome','$telefone','$idade','$extvocal','$endereco')");
+
+if($idade == "" || $idade == null){
+        echo"<script language='javascript' type='text/javascript'>alert('O campo idade deve ser preenchido');window.location.href='cadastromembros.html';</script>";
+}
+if($extvocal == "" || $extvocal == null){
+        echo"<script language='javascript' type='text/javascript'>alert('O campo extvocal deve ser preenchido');window.location.href='cadastromembros.html';</script>";
+}
+    if($nome == "" || $nome == null){
+        echo"<script language='javascript' type='text/javascript'>alert('O campo nome deve ser preenchido');window.location.href='cadastromembros.html';</script>";
+ 
+        }else{
+            if($logarray == $nome){
+ 
+                echo"<script language='javascript' type='text/javascript'>alert('Esse nome já existe');window.location.href='cadastromembros.html';</script>";
+                die();
+ 
+            }else{
+				$sql = mysql_query("INSERT INTO membros (nome,telefone,idade,ex_vocal,endereco) VALUES ('$nome','$telefone','$idade','$extvocal','$endereco')");
+                   
+                if($sql){
+                    echo"<script language='javascript' type='text/javascript'>alert('Membro cadastrado com sucesso!');window.location.href='login.php'</script>";
+                }else{
+                    echo"<script language='javascript' type='text/javascript'>alert('Não foi possível cadastrar esse membro');window.location.href='cadastromembros.html'</script>";
+                }
+            }
+        }
+?>
+
+
+
+
---
+++ membros/listar.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+
+<html lang="pt">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>Sistema Redenção</title>
+
+    <!-- Bootstrap -->
+    <!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="../bootstrap/css/bootstrap.min.css">
+
+
+<!-- Optional theme -->
+<link rel="stylesheet" href="../bootstrap/css/bootstrap-theme.min.css">
+<link rel="stylesheet" href="../bootstrap/css/estilos.css">
+
+<!-- Latest compiled and minified JavaScript -->
+<script src="../bootstrap/js/bootstrap.min.js"></script>
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+      <nav class="navbar navbar-inverse navbar-fixed-top">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#">Coral Jovem Redenção</a>
+        </div>
+        <div id="navbar" class="collapse navbar-collapse">
+          <ul class="nav navbar-nav">
+            <li class="active"><a href="#">Início</a></li>
+            <li><a href="membros/listar.php">Membros</a></li>
+            <li><a href="agenda/agenda.php">Agenda</a></li>
+            <li><a href="galeriafotos/galeriafotos.php">Galeria de Fotos</a></li>
+          </ul>
+        </div><!--/.nav-collapse -->
+      </div>
+    </nav>
+      <div class="container tema">
+           
+   <h1>Hello, world!</h1>
+      <a href="cadastromembros.html"> Cadastrar um membro
+      </div>
+      
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="bootstrap/js/bootstrap.min.js"></script>
+  </body>
+</html>
